1241468142da0c6db6d23c2ed93d91604615861a
Akshat123120 <74185066+Akshat123120@users.noreply.github.com>
2022-05-07 12:33:26 +0530

    Add files via upload

--- Music App/main.js
+++ Music App/main.js
@@ -6,6 +6,8 @@ rightWristX=0;
 rightWristY=0;
 song1status ="";
 song2status ="";
+scoreLeftWrist =0;
+scoreRightWrist=0;
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -22,14 +24,21 @@ function draw(){
     image(video,0,0,600,500);
     song1status= song1.isPlaying();
     song2status= song2.isPlaying();
+    fill("red");
+    stroke("red")
+    if(scoreLeftWrist>0.2){
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+        if(song2status==false){
+            song2.play();
+            document.getElementById("song").innerHTML = "playing - song 1";
+        }
+    }
 }
 function play(){
-    song1.play();
-    song2.play();
-    song1.setVolume(1);
-    song1.rate(1);
-    song2.setVolume(1);
-    song2.rate(1);
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 function modelLoaded(){
     console.log('poseNet is initialized');
@@ -40,6 +49,8 @@ function gotPoses(results){
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log('Left Wrist X = '+leftWristX+' and Left Wrist Y = '+leftWristY);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist="+scoreLeftWrist);
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
     }
